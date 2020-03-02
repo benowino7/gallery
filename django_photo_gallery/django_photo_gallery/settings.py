faeee602e634351ca73ca921f223c56bd7ece795
benowino7 <bensonowino7@gmail.com>
2020-03-02 08:35:31 +0300

    changed position of whitenoise on settings file

--- django_photo_gallery/django_photo_gallery/settings.py
+++ django_photo_gallery/django_photo_gallery/settings.py
@@ -39,12 +39,12 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
