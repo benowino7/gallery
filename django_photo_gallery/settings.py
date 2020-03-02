5e58a4d4c0880ca28b40a5b5278c21f602920da9
benowino7 <bensonowino7@gmail.com>
2020-03-02 10:47:38 +0300

    deployment to heroku

--- django_photo_gallery/settings.py
+++ django_photo_gallery/settings.py
@@ -9,7 +9,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('benjail')
+SECRET_KEY = 'benjail'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
